7f7d9bea5d196a791c4ab8ad8a2bf062803aa5c2
montylubana09 <montylubana51@gmail.com>
2023-02-04 14:48:10 -0700

    Card js working

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ function App() {
   return (
     <div className="App">
       <h1>CITI Hackathon</h1>
-      <Card />
+      <Card title="Hey" subtitle="wassup" imgalt="image1" />
+      <Card title="weey" subtitle="wassup" imgalt="image2" />
+      {/* <iframe
+        src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FLmjaNssVyk8qvjkR8ru5K3%2FYYC-HackApp-Design%3Fnode-id%3D18%253A33611%26scaling%3Dmin-zoom%26page-id%3D0%253A1%26starting-point-node-id%3D18%253A33611"
+        allowfullscreen
+        style={{ width: 800, height: 800 }}
+      /> */}
     </div>
   );
 }
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
-function card() {
-  return <div>card</div>;
+import Card from "react-bootstrap/Card";
+import Rating from "@mui/material/Rating";
+function card(props) {
+  return (
+    <div style={{ width: "18rem" }} class="shadow p-3 mb-3 rounded">
+      <Card.Body>
+        <Card.Title style={{ fontFamily: "Amatic SC" }}>Card Title</Card.Title>
+        <Card.Text style={{ fontFamily: "Montserrat" }}>
+          {props.subtitle}
+        </Card.Text>
+        <Rating name="read-only" value="4" readOnly />
+        <Card.Link href="#">Another Link</Card.Link>
+        <img src={props.img} alt={props.imgalt} />
+      </Card.Body>
+    </div>
+  );
 }
 
 export default card;
