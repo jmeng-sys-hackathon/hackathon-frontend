bdbeb5ab412836f0e2d77479c8596310e07ffd1c
Your Name <you@example.com>
2023-02-04 15:54:44 -0700

    fixing error with footer styling

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,61 +4,57 @@ import React from 'react';
 export default function Footer() {
   return (
     // Remove the container if you want to extend the Footer to full width.
-    <div class="container my-5">
+    <div className="container my-5">
 
-    {/* <!-- Footer --> */}
+  
     <footer
-          class="text-center text-lg-start text-white"
-          style="background-color: #1c2331"
+          className="text-center text-lg-start text-white"
+          style={{backgroundColor: "#1c2331"}}
           >
-    {/* <!-- Section: Social media --> */}
+ 
       <section
-              class="d-flex justify-content-between p-4"
-              style="background-color: #6351ce"
+              className="d-flex justify-content-between p-4"
+              style={{backgroundColor: "#6351ce"}}
         >
-        {/* <!-- Left --> */}
-        <div class="me-5">
+     
+        <div className="me-5">
           <span>Get connected with us on social networks:</span>
         </div>
-        {/* <!-- Left --> */}
-
-        {/* <!-- Right --> */}
+       
         <div>
-          <a href="" class="text-white me-4">
-            <i class="fab fa-facebook-f"></i>
+          <a href="" className="text-white me-4">
+            <i className="fab fa-facebook-f"></i>
           </a>
-          <a href="" class="text-white me-4">
-            <i class="fab fa-twitter"></i>
+          <a href="" className="text-white me-4">
+            <i className="fab fa-twitter"></i>
           </a>
-          <a href="" class="text-white me-4">
-            <i class="fab fa-google"></i>
+          <a href="" className="text-white me-4">
+            <i className="fab fa-google"></i>
           </a>
-          <a href="" class="text-white me-4">
-            <i class="fab fa-instagram"></i>
+          <a href="" className="text-white me-4">
+            <i className="fab fa-instagram"></i>
           </a>
-          <a href="" class="text-white me-4">
-            <i class="fab fa-linkedin"></i>
+          <a href="" className="text-white me-4">
+            <i className="fab fa-linkedin"></i>
           </a>
-          <a href="" class="text-white me-4">
-            <i class="fab fa-github"></i>
+          <a href="" className="text-white me-4">
+            <i className="fab fa-github"></i>
           </a>
         </div>
-        {/* <!-- Right --> */}
+     
       </section>
-      {/* <!-- Section: Social media --> */}
-
-      {/* <!-- Section: Links  --> */}
-      <section class="">
-        <div class="container text-center text-md-start mt-5">
-          {/* <!-- Grid row --> */}
-          <div class="row mt-3">
-            {/* <!-- Grid column --> */}
-            <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
-              {/* <!-- Content --> */}
-              <h6 class="text-uppercase fw-bold">Company name</h6>
+  
+      <section className="">
+        <div className="container text-center text-md-start mt-5">
+        
+          <div className="row mt-3">
+          
+            <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+             
+              <h6 className="text-uppercase fw-bold">Company name</h6>
               <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
+                  className="mb-4 mt-0 d-inline-block mx-auto"
+                  style={{width: "60px", backgroundColor: "#7c4dff", height: "2px"}}
                   />
               <p>
                 Here you can use rows and columns to organize your footer
@@ -66,87 +62,76 @@ export default function Footer() {
                 elit.
               </p>
             </div>
-            {/* <!-- Grid column --> */}
-
-            {/* <!-- Grid column --> */}
-            <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
-              {/* <!-- Links --> */}
-              <h6 class="text-uppercase fw-bold">Products</h6>
+           
+            <div className="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            
+              <h6 className="text-uppercase fw-bold">Products</h6>
               <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
+                  className="mb-4 mt-0 d-inline-block mx-auto"
+                  style={{width: "60px", backgroundColor: "#7c4dff", height: "2px"}}
                   />
               <p>
-                <a href="#!" class="text-white">MDBootstrap</a>
+                <a href="#!" className="text-white">MDBootstrap</a>
               </p>
               <p>
-                <a href="#!" class="text-white">MDWordPress</a>
+                <a href="#!" className="text-white">MDWordPress</a>
               </p>
               <p>
-                <a href="#!" class="text-white">BrandFlow</a>
+                <a href="#!" className="text-white">BrandFlow</a>
               </p>
               <p>
-                <a href="#!" class="text-white">Bootstrap Angular</a>
+                <a href="#!" className="text-white">Bootstrap Angular</a>
               </p>
             </div>
-            {/* <!-- Grid column --> */}
-
-            {/* <!-- Grid column --> */}
-            <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
-              {/* <!-- Links --> */}
-              <h6 class="text-uppercase fw-bold">Useful links</h6>
+      
+            <div className="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+             
+              <h6 className="text-uppercase fw-bold">Useful links</h6>
               <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
+                  className="mb-4 mt-0 d-inline-block mx-auto"
+                  style={{width: "60px", backgroundColor: "#7c4dff", height: "2px"}}
                   />
               <p>
-                <a href="#!" class="text-white">Your Account</a>
+                <a href="#!" className="text-white">Your Account</a>
               </p>
               <p>
-                <a href="#!" class="text-white">Become an Affiliate</a>
+                <a href="#!" className="text-white">Become an Affiliate</a>
               </p>
               <p>
-                <a href="#!" class="text-white">Shipping Rates</a>
+                <a href="#!" className="text-white">Shipping Rates</a>
               </p>
               <p>
-                <a href="#!" class="text-white">Help</a>
+                <a href="#!" className="text-white">Help</a>
               </p>
             </div>
-            {/* <!-- Grid column --> */}
-
-            {/* <!-- Grid column --> */}
-            <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-              {/* <!-- Links --> */}
-              <h6 class="text-uppercase fw-bold">Contact</h6>
+           
+            <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            
+              <h6 className="text-uppercase fw-bold">Contact</h6>
               <hr
-                  class="mb-4 mt-0 d-inline-block mx-auto"
-                  style="width: 60px; background-color: #7c4dff; height: 2px"
+                  className="mb-4 mt-0 d-inline-block mx-auto"
+                  style={{width: "60px", backgroundColor: "#7c4dff", height: "2px"}}
                   />
-              <p><i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
-              <p><i class="fas fa-envelope mr-3"></i> info@example.com</p>
-              <p><i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-              <p><i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+              <p><i className="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+              <p><i className="fas fa-envelope mr-3"></i> info@example.com</p>
+              <p><i className="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+              <p><i className="fas fa-print mr-3"></i> + 01 234 567 89</p>
             </div>
-            {/* <!-- Grid column --> */}
+       
           </div>
-          {/* <!-- Grid row --> */}
+         
         </div>
       </section>
-      {/* <!-- Section: Links  --> */}
-
-    {/* <!-- Copyright --> */}
     <div
-         class="text-center p-3"
-         style="background-color: rgba(0, 0, 0, 0.2)"
+         className="text-center p-3"
+         style={{backgroundColor: "rgba(0, 0, 0, 0.2)"}}
          >
       © 2020 Copyright:
-      <a class="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+      <a className="text-white" href="https://mdbootstrap.com/">MDBootstrap.com</a>
     </div>
-    {/* <!-- Copyright --> */}
+ 
   </footer>
-  {/* <!-- Footer --> */}
 
   </div>
-  {/* <!-- End of .container --> */}
   );
 };
