c7366e36e27bb7a0cad4b8ad60c36e8fe25036b6
montylubana09 <montylubana51@gmail.com>
2023-02-04 19:34:36 -0700

    commit update

--- src/Pages/Form2.jsx
+++ src/Pages/Form2.jsx
@@ -4,7 +4,11 @@ import Card from "../Components/Card";
 function Form2(props) {
   return (
     <div>
-      <Card subtitle={props.img} />
+      <label>You picked Sports! Do want to participate or view?</label>
+      <div className="cardspec" style={{ flexDirection: "row" }}>
+        <Card subtitle={props.img} title="Participate!" />
+        <Card subtitle={props.img} title="Spectate!" />
+      </div>
     </div>
   );
 }
